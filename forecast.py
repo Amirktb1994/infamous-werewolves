d40e30a799114d31bf139de5d789c0523640fe06
ng3223@kit.edu <ng3223@hkn1703.localdomain>
2022-02-03 16:44:29 +0100

    updates

--- forecast.py
+++ forecast.py
@@ -1,68 +1,45 @@
 import os
-from argparse import ArgumentParser
-import torch
-from torch.utils.data import DataLoader
-from pandas import DataFrame
-from dataset import CustomLoadDataset
+import pickle
+from features import *
 
-# TODO: import your model
-from model import LoadForecaster as SubmittedModel
+pd.options.mode.chained_assignment = None
 
+with open('model_dict.pickle' , 'rb') as f:
+    model = pickle.load(f)
+    
+data_dir = '/hkfs/work/workspace/scratch/bh6321-energy_challenge/data/'
+save_dir = 'forecasts/'
 
-def forecast(forecast_model, forecast_set, device):
-    forecast_model.to(device)
-    forecast_model.eval()
+# dataloader
+test_file = os.path.join(data_dir, 'test.csv')
+valid_file = os.path.join(data_dir, 'valid.csv')
 
-    batch_size = 64
-    forecast_loader = DataLoader(forecast_set, batch_size=64, shuffle=False)
-    forecasts = torch.zeros([len(forecast_set), 7*24], device=device)
-    for n, (input_seq, _) in enumerate(forecast_loader):
-        # TODO: adjust forecast loop according to your model
-        with torch.no_grad():
-            actual_batch_size = len(input_seq)  # last batch has different size
-            hidden = forecast_model.init_hidden(actual_batch_size)
-            prediction, hidden = forecast_model(input_seq, hidden)
-            forecasts[n * batch_size:n * batch_size + actual_batch_size] = prediction.squeeze(dim=-1)
-    return forecasts
+data_file = test_file if os.path.exists(test_file) else valid_file
 
+df = pd.read_csv(data_file)
+df = preprocess(df)
+df = add_time_features(df, drop = True)
+df.drop(columns='day_name', inplace=True)
+df_list = add_ts_features(df, return_as_list=True)
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--weights_path", type=str,
-                        default='/hkfs/work/workspace/scratch/bh6321-energy_challenge/AI-HERO/energy_baseline.pt',
-                        help="Model weights path")  # TODO: adapt to your model weights path
-    parser.add_argument("--save_dir", type=str, help='Directory where weights and results are saved', default='.')
-    parser.add_argument("--data_dir", type=str, help='Directory containing the data you want to predict',
-                        default='/hkfs/work/workspace/scratch/bh6321-energy_challenge/data')
-    args = parser.parse_args()
+forecast_dict = dict()
+for df_tmp in df_list:
+    city = df_tmp.city.unique()[0]
+    print(city)
+    df_tmp.drop(columns='city', inplace=True)
+    pred = model[city].predict(df_tmp)
+    forecast_dict[city] = pred
+    
+y_pred = np.array([])
+for f in forecast_dict.values():
+    f = f[:-24*7]
+    for i in range(8424):
+        y_pred = np.concatenate([y_pred, f[i : i+168]])
+        
+submission = pd.DataFrame(np.reshape(y_pred, (len(y_pred)//(24*7), 24*7)))
 
-    save_dir = args.save_dir
-    data_dir = args.data_dir
+# save to csv
+result_path = os.path.join(save_dir, 'forecasts.csv')
+submission.to_csv(result_path, header=False, index=False)
 
-    weights_path = args.weights_path
-
-    # load model with pretrained weights
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # TODO: adjust arguments according to your model
-    model = SubmittedModel(input_size=1, hidden_size=48, output_size=1, num_layer=1, device=device)
-    model.load_state_dict(torch.load(weights_path, map_location=device))
-    model.eval()
-
-    # dataloader
-    test_file = os.path.join(data_dir, 'test.csv')
-    valid_file = os.path.join(data_dir, 'valid.csv')
-    data_file = test_file if os.path.exists(test_file) else valid_file
-    testset = CustomLoadDataset(data_file, 7*24, 7*24, device=device)
-
-    # run inference
-    normalized_forecasts = forecast(model, testset, device)
-
-    # remove normalization and convert to DataFrame
-    forecasts = testset.revert_normalization(normalized_forecasts)
-    df = DataFrame(forecasts.to(torch.device('cpu')).numpy())
-
-    # save to csv
-    result_path = os.path.join(save_dir, 'forecasts.csv')
-    df.to_csv(result_path, header=False, index=False)
-
-    print(f"Done! The result is saved in {result_path}")
+print(f"Done! The result is saved in {result_path}")
